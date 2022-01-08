668eb8a5a77c3ce04158f7ed705e9501e60f8437
betolarbac <betocr.dev@gmail.com>
2022-01-07 21:42:25 -0300

    finalizando header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import {Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import {Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 
 export function Header() {
@@ -47,6 +47,37 @@ export function Header() {
                 />
 
                 <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+
+            <Flex
+                align="center"
+                ml="auto"
+            >
+
+              <HStack
+                spacing="8"
+                mx="8"
+                pr="8"
+                py="1"
+                color="gray.300"
+                borderRightWidth={1}
+                borderColor="gray.700"
+              >
+              <Icon as={RiNotificationLine} fontSize="20" />
+                <Icon as={RiUserAddLine} fontSize="20" />
+              </HStack>
+              
+              <Flex align="center" >
+                  <Box mr="4" textAlign="right">
+                    <Text>Roberto Cabral</Text>
+                    <Text color="gray.300" fontSize="small">
+                        roberto_jr123@hotmail.com
+                    </Text>
+                  </Box>
+                
+                  <Avatar size="md" name="Roberto Cabral" src="https://github.com/betolarbac.png" />
+
+              </Flex>
             </Flex>
         </Flex>
     );
