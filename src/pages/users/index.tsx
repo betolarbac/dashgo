e9f2a7b96907cf357dc2b215309c1ee68f00dd17
betolarbac <betocr.dev@gmail.com>
2022-01-11 18:49:00 -0300

    componente de paginação

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th,
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
+import Pagination from "../../components/Pagination";
 import { Siderbar } from "../../components/Sidebar";
 
 export default function UserList() {
@@ -115,6 +116,7 @@ export default function UserList() {
                             </Tr>
                         </Tbody>
                     </Table>
+                    <Pagination />
                 </Box>
             </Flex>
         </Box>
