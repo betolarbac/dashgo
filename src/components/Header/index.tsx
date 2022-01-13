89e60abe0e3a35b77e2fce448ef7b04b5a92981a
betolarbac <betocr.dev@gmail.com>
2022-01-13 20:23:07 -0300

    responsividade de sidebar e criação de menu hamburguer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import {Flex, useBreakpointValue } from '@chakra-ui/react'
+import {Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContexts';
 import { Logo } from './Logo';
 import { NotificationNav } from './NotificationNav';
 import { Profile } from './Profile';
@@ -6,6 +8,8 @@ import { SearchBox } from './SearchBox';
 
 
 export function Header() {
+    const {onOpen} = useSidebarDrawer()
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
@@ -21,8 +25,23 @@ export function Header() {
         px="7"
         align="center"
         >
+            {!isWideVersion && (
+                <IconButton
+                    aria-label="Open Navegation"
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    mr="2"
+                >
+
+                </IconButton>
+            )}
+
             <Logo />
-            <SearchBox />
+
+           {isWideVersion && <SearchBox />}
+
             <Flex
                 align="center"
                 ml="auto"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 
+import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContexts'
+
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
         <ChakraProvider  theme={theme}>
+          <SidebarDrawerProvider>
            <Component {...pageProps} />
+           </SidebarDrawerProvider>
         </ChakraProvider>
     )
 }
