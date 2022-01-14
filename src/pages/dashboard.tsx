51d979ddc1c5fe6924b62ef10d4111088c1e12d7
betolarbac <betocr.dev@gmail.com>
2022-01-13 21:23:10 -0300

    responsividade dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -57,7 +57,7 @@ export default function Dashboard() {
 
                 <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
                     <Box
-                      p="8"
+                      p={["6", "8"]}
                       bg="gray.800"
                       borderRadius={8}
                       pb="4"
@@ -68,7 +68,7 @@ export default function Dashboard() {
                     </Box>
 
                     <Box
-                      p="8"
+                      p={["6", "8"]}
                       bg="gray.800"
                       borderRadius={8}
                     >
