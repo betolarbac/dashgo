e3f233e68a09e40b54fb68c62f14b3c808cbd29b
betolarbac <betocr.dev@gmail.com>
2022-01-17 20:15:29 -0300

    form de autenticação

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,31 +1,43 @@
-import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
+import { FieldError } from "react-hook-form";
+import {
+  Input as FormInput,
+  FormLabel,
+  FormControl,
+  InputProps as ChakraInputProps,
+  FormErrorMessage
+} from "@chakra-ui/react";
 
 interface InputProps extends ChakraInputProps {
-    name: string;
-    label?: string;
-
+  name: string;
+  label?: string;
+  error?: FieldError;
 }
 
-export function Input({name, label, ...rest}: InputProps) {
-    return (
-        <FormControl>
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error = null, ...rest },
+  ref
+) => {
+  return (
+    <FormControl isInvalid={!!error}>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <FormInput
+        name={name}
+        id={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: "gray.900"
+        }}
+        size="lg"
+        ref={ref}
+        {...rest}
+      />
 
-            { !!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+    </FormControl>
+  );
+};
 
-              <ChakraInput
-              name={name} 
-              id={name}
-              type="email" 
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-              {...rest}
-
-              />
-           </FormControl>
-    );
-}
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import { Flex, Button, Stack,  } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Input } from '../components/Form/Input'
 
+
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
 export default function SignIn() {
+  const { register, handleSubmit, formState} = useForm()
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log(values);
+  }
+
   return (
     <Flex
     w="100vw"
@@ -17,14 +31,23 @@ export default function SignIn() {
       p="8"
       borderRadius={8}
       flexDirection="column"
+      onSubmit={handleSubmit(handleSignIn)}
       >
          <Stack spacing="4">
 
-           <Input name="email" type="email" label="E-mail" />
-           <Input name="password" type="password" label="Senha" />
+           <Input name="email" type="email" label="E-mail" {...register('email')}/>
+           <Input name="password" type="password" label="Senha" {...register('password')} />
 
            
-            <Button type="submit" mt="6" colorScheme="pink" size="lg" > Entrar </Button>
+            <Button 
+            type="submit" 
+            mt="6" 
+            colorScheme="pink" 
+            size="lg"
+            isLoading={formState.isSubmitting} 
+            > 
+            Entrar 
+            </Button>
          </Stack>
       </Flex>
     </Flex>
